98e527674b01813ac2fb24567000bdbd8f867d57
Kristina Guseynova <bittersweet2010@list.ru>
2024-09-14 22:50:28 +0400

    added PracticeFormTests

---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,63 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Mr");
+        $("#lastName").setValue("Smith");
+        $("#userEmail").setValue("test@test.xyz");
+        $(byText("Female")).click();
+        $("#userNumber").setValue("79991112233");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("March")).click();
+        $(".react-datepicker__year-select").$(byText("2020")).click();
+        $("div.react-datepicker__day.react-datepicker__day--010").click();
+        $("#subjectsInput").setValue("eng").pressEnter();
+        $(byText("Reading")).scrollTo().click();
+        $("#uploadPicture").uploadFile(new File("C:\\Users\\bitte\\OneDrive\\Рабочий стол\\мульти пульти\\1.png"));
+        $("#currentAddress").setValue("221B Baker Street");
+        $("#state").click();
+        $("#react-select-3-option-2").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+        $("#submit").click();
+
+
+        $(".table-responsive").shouldHave(text("Mr Smith"));
+        $(".table-responsive").shouldHave(text("test@test.xyz"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("10 March,2020"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("1.png"));
+        $(".table-responsive").shouldHave(text("221B Baker Street"));
+        $(".table-responsive").shouldHave(text("Haryana Panipat"));
+
+        try {
+            sleep(10000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
